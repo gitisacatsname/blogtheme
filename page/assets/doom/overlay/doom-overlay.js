b9d0828f19bd1b56e4a99d56de5a8fdf8d3387b2
Constantin Korte <94610393+gitisacatsname@users.noreply.github.com>
2025-08-27 21:31:33 +0200

    Remove game selector and streamline DOOM overlay

--- page/assets/doom/overlay/doom-overlay.js
+++ page/assets/doom/overlay/doom-overlay.js
@@ -6,9 +6,7 @@
     const btn   = qs('.doom-open', root);
     const wrap  = qs('#doom-frame-wrap', root);
     const frame = qs('#doom-frame', root);
-    const btnFS = qs('.doom-fullscreen', root);
     const btnClose = qs('.doom-close', root);
-    const selIwad = qs('.doom-iwad', root);
 
     let lastFocus = null;
 
@@ -26,30 +24,11 @@
     function open() {
       lastFocus = document.activeElement;
       wrap.hidden = false;
-      const game = encodeURIComponent(selIwad.value);
-      const url = DOOM_OVERLAY_CFG.engineUrl + '?game=' + game;
-      frame.src = url;
+      frame.src = DOOM_OVERLAY_CFG.engineUrl;
       frame.focus();
     }
 
     btn.addEventListener('click', open);
-
-    selIwad.addEventListener('change', () => {
-      const game = encodeURIComponent(selIwad.value);
-      frame.src = DOOM_OVERLAY_CFG.engineUrl + '?game=' + game;
-      focusFrame();
-    });
-
-    btnFS.addEventListener('click', () => {
-      const mod = frame.contentWindow?.Module;
-      if (typeof mod?.requestFullscreen === 'function') {
-        mod.requestFullscreen(true, false);
-      } else {
-        const fs = frame.contentDocument?.getElementById('fullscreen');
-        fs?.click();
-      }
-      focusFrame();
-    });
 
     btnClose.addEventListener('click', () => {
       wrap.hidden = true;
